9f5bead9fa58b87fa9914a9a9a577bc20ff51ccf
b2logicfinder <132153222+b2logicfinder@users.noreply.github.com>
2023-12-27 19:41:09 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,61 +100,13 @@ resource "azurerm_network_interface" "networkfort_nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "networkfort" {
-  name                = "networkfort"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.testingresource.name
+  name                  = "networkfort"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.testingresource.name
   network_interface_ids = [azurerm_network_interface.networkfort_nic.id]
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  disable_password_authentication = false
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-}
-
-resource "azurerm_public_ip" "networkfort_tester_pip" {
-  name                = "networkfortTesterPublicIP"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.testingresource.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "networkfort_tester_nic" {
-  name                = "networkfortTesterNIC"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.testingresource.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.networkfort_tester_pip.id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "networkfort_tester_nsg_association" {
-  network_interface_id      = azurerm_network_interface.networkfort_tester_nic.id
-  network_security_group_id = azurerm_network_security_group.networkfort_nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "networkfort_tester" {
-  name                = "networkfort-tester"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.testingresource.name
-  network_interface_ids = [azurerm_network_interface.networkfort_tester_nic.id]
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  size                  = "Standard_B1s"
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   disable_password_authentication = false
 
   source_image_reference {
@@ -176,13 +128,13 @@ resource "azurerm_virtual_machine_extension" "networkfort_init" {
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
-    timeouts {
+  timeouts {
     create = "30m"
   }
 
-  settings = <<SETTINGS
+ settings = <<SETTINGS
     {
-      "commandToExecute": "echo '#!/bin/bash\n\nsudo apt-get install apt-transport-https\n\n# Step 1 — Installing Elasticsearch and Kibana\n\ncurl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -\n\necho \"deb https://artifacts.elastic.co/packages/7.x/apt stable main\" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list\n\n# Update package lists\nsudo apt-get update\n\n# Install ELK Stack (Elasticsearch, Logstash, Kibana)\nsudo apt-get install -y default-jre\nsudo apt-get install -y elasticsearch logstash kibana filebeat\n\n# Start ELK Stack services\nsudo service elasticsearch start\nsudo service logstash start\nsudo service kibana start\n\nsudo filebeat modules enable suricata\nsudo filebeat modules enable zeek\n\nsudo filebeat setup\n\nsudo systemctl start filebeat.service\n\n# Install Suricata\n\nadd-apt-repository ppa:oisf/suricata-stable\n\nsudo apt-get install -y suricata\n\n# Start Suricata service\nsudo service suricata start\nsudo systemctl enable suricata\n\n# Install Zeek\n\necho 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_20.10/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list\ncurl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_20.10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null\napt update\n\nsudo apt-get install -y zeek\n\n# Start Zeek service\nsudo zeekctl deploy\nsudo zeekctl start\n\n# Add any additional configuration or setup steps as needed\n\n# End of installation script' > install.sh && chmod +x install.sh && sudo ./install.sh"
+       "commandToExecute": "curl -o install.sh https://raw.githubusercontent.com/b2logicfinder/terraformform-script/main/install.sh && sudo chmod +x ./install.sh && sudo ./install.sh"
     }
 SETTINGS
 }
