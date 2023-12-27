379c34989b8fef930d5cde8c1aaeeee63b04f5f5
b2logicfinder <132153222+b2logicfinder@users.noreply.github.com>
2023-12-27 19:03:35 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+sudo apt install git -y
+
+
 sudo apt-get install apt-transport-https
 
 # Step 1 — Installing Elasticsearch and Kibana
@@ -55,5 +59,27 @@ sudo zeekctl deploy
 sudo zeekctl start
 
 # Add any additional configuration or setup steps as needed
-
+# Define URLs for the raw configuration files from GitHub\n
+        ELASTICSEARCH_CONFIG_URL='https://raw.githubusercontent.com/user/repo/branch/path/to/elasticsearch.yml'
+        ZEEK_CONFIG_URL='https://raw.githubusercontent.com/user/repo/branch/path/to/zeek/node.cfg'
+        SURICATA_CONFIG_URL='https://raw.githubusercontent.com/user/repo/branch/path/to/suricata/suricata.yaml'
+        LOGSTASH_CONFIG_URL='https://raw.githubusercontent.com/user/repo/branch/path/to/logstash/logstash.conf'
+        KIBANA_CONFIG_URL='https://raw.githubusercontent.com/user/repo/branch/path/to/kibana/kibana.yml'
+        # Download and replace the Elasticsearch configuration file\n
+        curl -o /etc/elasticsearch/elasticsearch.yml $ELASTICSEARCH_CONFIG_URL
+        # Download and replace the Zeek configuration file\n
+        curl -o /usr/local/zeek/etc/node.cfg $ZEEK_CONFIG_URL
+        # Download and replace the Suricata configuration file\n
+        curl -o /etc/suricata/suricata.yaml $SURICATA_CONFIG_URL
+ # Download and replace the Logstash configuration file\n
+        curl -o /etc/logstash/conf.d/logstash.conf $LOGSTASH_CONFIG_URL
+        # Download and replace the Kibana configuration file\n
+        curl -o /etc/kibana/kibana.yml $KIBANA_CONFIG_URL
+        # Restart services to apply new configurations\n
 # End of installation script
+# Restart services to apply new configurations\n
+        sudo systemctl restart elasticsearch\n
+        sudo systemctl restart kibana\n
+        sudo systemctl restart logstash\n
+        sudo zeekctl restart\n
+        sudo systemctl restart suricata\n
